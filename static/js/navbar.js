b3b02a2cd32e3d6164a6fbb8379282dad080994f
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2026-01-26 22:12:40 +0100

    add features and navbar pages

---
+++ static/js/navbar.js
@@ -0,0 +1,7 @@
+const navbar = document.getElementById("navbar-thing");
+
+fetch("/static/html/navbar.html")
+    .then(response => response.text())
+    .then(html => {
+        navbar.innerHTML = html;
+    });
